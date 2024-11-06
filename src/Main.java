cb4d3da73a6565639f4a38bc4f9c59835b5d61eb
/artem kasapov <kasapova79@gmail.com>
2024-11-06 15:00:32 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,121 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Dev. artem.kasapov (@Nebulart-dev) - Java//
+
+/*
+// Задание 1
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int height;
+        height = 186;
+        System.out.print("Рост: " + height + " см");
     }
 }
+*/
+
+/*
+//Задание 2
+public class Main {
+    public static void main(String[] args) {
+        int length, width;
+        length = 15;
+        width = 25;
+        System.out.print(length + " & " + width);
+    }
+}
+*/
+
+/*
+//Задание 3
+public class Main {
+    public static void main(String[] args) {
+        double price;
+        price = 99.99;
+        System.out.println("Первоначальн. значение: " + price);
+        price = 79.99;
+        System.out.print("Измененное значение: " + price);
+    }
+}
+*/
+
+/*
+//Задание 4
+public class Main {
+    public static void main(String[] args) {
+        number;
+        number = 100;
+        System.out.println(number);
+        //Ответ: Вывод number без присвоения переменной - невозможен!
+    }
+}
+*/
+
+/*
+//Задание 5
+public class Main {
+    public static void main(String[] args) {
+        String city;
+        city = "Краснодар";
+        System.out.println("Город: " + city);
+    }
+}
+*/
+
+/*
+//Задание 6
+public class Main {
+    public static void main(String[] args) {
+        int day, month, year;
+        day = 20;
+        month = 10;
+        year = 2024;
+        System.out.println(day + " " + month + " " + year + " ");
+    }
+}
+*/
+
+/*
+//Задание 7
+public class Main {
+    public static void main(String[] args) {
+        double radius, x;
+        radius = 7.5;
+        x = Math.PI * radius * radius;
+        System.out.println("Результат: " + x);
+    }
+}
+*/
+
+/*
+//Задание 8
+public class Main {
+    public static void main(String[] args) {
+        boolean isJavaFun;
+        isJavaFun = true;
+        System.out.println(isJavaFun);
+    }
+}
+*/
+
+/*
+//Задание 9
+public class Main {
+    public static void main(String[] args) {
+        int count;
+        count = 0;
+        count =+ 10;
+        count -= 3;
+        System.out.println("Результат: " + count);
+    }
+}
+*/
+
+/*
+//Задание 10
+public class Main {
+    public static void main(String[] args) {
+        String firstName, lastName;
+        firstName = "Артем";
+        lastName = "Касапов";
+        System.out.println(firstName + " " + lastName);
+    }
+}
+*/
